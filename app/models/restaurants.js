7c097de546bf79932e1e87629d14b75985ea1707
Harish Navnit <harishnavnit@gmail.com>
2022-09-05 23:16:43 +0530

    Fix ETL

--- app/models/restaurants.js
+++ app/models/restaurants.js
@@ -6,10 +6,10 @@ const { DataTypes: FIELDS } = require('sequelize')
 const options = { timestamps: true, tableName: 'restaurants' }
 const attributes = {
     menu: { type: FIELDS.JSON },
-    name: { type: FIELDS.STRING, allowNull: true }, // TODO: {allowNull: false}
-    balance: { type: FIELDS.FLOAT, field: 'cashBalance' },
-    timings: { type: FIELDS.JSON, field: 'openingHours' },
-    id: { type: FIELDS.UUID, defaultValue: FIELDS.UUIDV4, primaryKey: true }
+    timings: { type: FIELDS.JSON },
+    balance: { type: FIELDS.FLOAT },
+    name: { type: FIELDS.STRING, allowNull: false },
+    id: { type: FIELDS.INTEGER, autoIncrement: true, primaryKey: true }
 }
 
 exports.Restaurants = (db) => db.define('Restaurants', attributes, options)
--- data/store.js
+++ data/store.js
@@ -9,36 +9,59 @@ const { Users } = require('../app/models/users')
 const { Restaurants } = require('../app/models/restaurants')
 
 
-const connection = new Sequelize({
-    dialect: 'sqlite', storage: path.resolve(__dirname, '..', 'dev.sqlite')
-})
+const data = {
+    users: () => {
+        const content = fs.readFileSync(path.resolve(__dirname, 'users.json'), 'utf-8')
+        return Object.values(JSON.parse(content))
+            .map(item => Object({
+                'name': item.name,
+                'balance': item.cashBalance,
+                'purchases': item.purchaseHistory,
+            }))
+    },
 
-const shorten = (rows) => rows.splice(0, 5000)
-
-const data = (target) => fs.readFileSync(path.resolve(__dirname, `${target}.json`), 'utf-8')
-
-const up = () => {
-    const u = Users(connection),
-          r = Restaurants(connection)
-
-    u.sync()
-        .catch(err => console.error(err))
-        .then(res => {
-            console.log('\nPopulating users database ...')
-            u.bulkCreate(shorten(Object.values(data('users'))))
-        })
-    r.sync()
-        .catch(err => console.error(err))
-        .then(res => {
-            console.log('\nPopulating restaurants database ...')
-            u.bulkCreate(shorten(Object.values(data('restaurants'))))
-        })
+    restaurants: () => {
+        const content = fs.readFileSync(path.resolve(__dirname, 'restaurants.json'), 'utf-8')
+        return Object.values(JSON.parse(content))
+            .map(item => Object({
+                'menu': item.menu,
+                'name': item.restaurantName,
+                'balance': item.cashBalance,
+                'timings': item.openingHours,
+            }))
+    }
 }
 
-const down = () => connection.close()
+
+// Connect to a database and define model instances
+const connection = new Sequelize({
+    logging: false,
+    dialect: 'sqlite',
+    storage: path.resolve(__dirname, '..', 'dev.sqlite')
+})
+const u = Users(connection)
+const r = Restaurants(connection)
+
+// Populate database with raw data
+const up = () => {
+    u.sync()
+        .catch(err => console.error(err))
+        .then(res => u.bulkCreate(data.users()))
+    r.sync()
+        .catch(err => console.error(err))
+        .then(res => r.bulkCreate(data.restaurants()))
+}
+
+// Cleanup the database
+const down = () => {
+    u.destroy({ truncate: true, cascade: true })
+    r.destroy({ truncate: true, cascade: true })
+    connection.close()
+}
 
 
 module.exports = { up, down, connection }
+
 
 if (require.main === module) {
     connection.sync({force: true})
