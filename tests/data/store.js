3d8ee99ebe6b414f3c686d4b9b4f4d8f0dfa3cec
Harish Navnit <harish.navnit@dialpad.com>
2022-09-07 10:34:25 +0530

    Migrate to jest

--- tests/data/store.js
+++ tests/data/store.js
@@ -1,59 +1,71 @@
 'use strict'
 
-const test = require('blue-tape')
+const path = require('path')
+const { Sequelize } = require('sequelize')
 
 const store = require('../../data/store')
 const { Users } = require('../../app/models/users')
 const { Restaurants } = require('../../app/models/restaurants')
 
-const u = Users(store.connection),
-      r = Restaurants(store.connection)
+
+const db = {}
 
 
-test('Users model insertion', async (t) => {
-    const before = await u.findAll()
-    t.notEqual(before.length, store.data.users().length)
+beforeEach(async () => {
+    db.users = Users(store.connection)
+    await db.users.sync({force: true})
 
-    await store.up()
-
-    const after = await u.findAll()
-    t.equal(after.length, store.data.users().length)
+    db.restaurants = Restaurants(store.connection)
+    await db.users.sync({force: true})
 })
 
-test('Restaurants model insertion', async (t) => {
-    const before = await r.findAll()
-    t.notEqual(before.length, store.data.restaurants().length)
-
-    await store.up()
-
-    const after = await r.findAll()
-    t.equal(after.length, store.data.restaurants().length)
+afterEach(async () => {
+    delete db.users
+    delete db.restaurants
 })
 
-test('Users model cleanup', async (t) => {
-    // Populate the database
-    await store.up()
+describe('Users model population', () => {
+    test('Users model insertion', async () => {
+        const before = await db.users.findAll()
+        expect(before.length).toEqual(0)
 
-    const before = await u.findAll()
-    t.assert(before.length)
+        await store.up()
+        const after = await db.users.findAll()
+        expect(after.length).toBeGreaterThan(0)
+    })
 
-    // Cleanup the database
-    await store.down()
+    test('Users model cleanup', async () => {
+        // Populate the database
+        await store.up()
+        expect(db.users.findAll()).resolves.toBeDefined()
 
-    const after = await u.findAll()
-    t.equal(after.length, 0)
+        // Cleanup the database
+        await store.down()
+        expect(db.users.findAll()).rejects.toBeDefined()
+    })
 })
 
-test('Restaurants model cleanup', async (t) => {
-    // Populate the database
-    await store.up()
+describe('Restaurants model population', () => {
+    test('Restaurants model insertion', async () => {
+        const before = await db.restaurants.findAll()
+        expect(before.length).toBeLessThan(store.data.restaurants().length)
 
-    const before = await r.findAll()
-    t.assert(before)
+        await store.up()
 
-    // Cleanup the database
-    await store.down()
+        const after = await db.restaurants.findAll()
+        expect(after.length).toEqual(store.data.restaurants().length)
+    })
 
-    const after = await r.findAll()
-    t.equal(after.length, 0)
+    test('Restaurants model cleanup', async () => {
+        // Populate the database
+        await store.up()
+        const before = await db.restaurants.findAll()
+        expect(before.length).toBeGreaterThan(0)
+
+        // Cleanup the database
+        await store.down()
+        const after = await db.restaurants.findAll()
+        expect(after.length).toEqual(0)
+        // try { await db.restaurants.findAll() } catch (err) { expect(err).not.toBeNull() }
+    })
 })
