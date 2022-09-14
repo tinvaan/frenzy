3206485d4ad1fd61beb987de6452325594774f77
Harish Navnit <harish.navnit@dialpad.com>
2022-09-14 08:09:06 +0530

    Update configs

--- config/development.js
+++ config/development.js
@@ -6,6 +6,7 @@ const path = require('path')
 module.exports = {
     service: {
         port: 3000,
+        name: 'frenzy',
         root: path.resolve(__dirname, '..')
     },
     database: {
--- config/testing.js
+++ config/testing.js
@@ -8,6 +8,7 @@ process.env.NODE_ENV = 'testing'
 module.exports = {
     service: {
         port: 3000,
+        name: 'frenzy',
         root: path.resolve(__dirname, '..')
     },
     database: {
