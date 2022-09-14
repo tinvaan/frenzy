db0e336735ecf55d86a14f1066ad7d0400cfdc89
Harish Navnit <harish.navnit@dialpad.com>
2022-09-14 08:09:39 +0530

    Modularize startup scripts

--- app/index.js
+++ app/index.js
@@ -1,34 +1,60 @@
 'use strict'
 
+const config = require('config')
 const restify = require('restify')
+
 const router = require('./routes')
 
-const server = restify.createServer({
-    name: 'frenzy',
-    ignoreTrailingSlash: true,
-    formatters: {
-        'application/json': (req, res, body) => JSON.stringify(body, null, 2)
-    }
-})
+
+let server
+
+const init = async () => {
+    if (server !== undefined) { return server }
+
+    server = restify.createServer({
+        name: config.get('service.name'),
+        ignoreTrailingSlash: true,
+        formatters: {
+            'application/json': (req, res, body) => JSON.stringify(body, null, 2)
+        }
+    })
+
+    // Initialize pre-routing plugins
+    server.pre(restify.plugins.pre.dedupeSlashes())
+
+    // Initialize server plugins
+    server.use(restify.plugins.jsonp())
+    server.use(restify.plugins.bodyParser())
+    server.use(restify.plugins.queryParser())
+
+    // Initialize app routes
+    router.applyRoutes(server)
+
+    // Log requests and responses to console
+    server.pre((req, res, next) => {
+        console.log(`\n>>> ${res.statusCode} ${req.method} ${req.url}`)
+        next()
+    })
+    server.on('after', (req, res, next) => console.log(res._data))
+
+    return server
+}
+
+const start = async () => {
+    // Initialize the server
+    await init()
+
+    // Run the app server
+    return server.listen(config.get('service.port'), async () => {
+        console.log(`${server.name} listening at ${server.url}`)
+    })
+}
+
+const stop = async () => server.close()
 
 
-// Initialize pre-routing plugins
-server.pre(restify.plugins.pre.dedupeSlashes())
-
-// Initialize server plugins
-server.use(restify.plugins.jsonp())
-server.use(restify.plugins.bodyParser())
-server.use(restify.plugins.queryParser())
-
-// Initialize app routes
-router.applyRoutes(server)
-
-// Log requests and responses to console
-server.pre((req, res, next) => {
-    console.log(`\n>>> ${res.statusCode} ${req.method} ${req.url}`)
-    next()
-})
-server.on('after', (req, res, next) => console.log(res._data))
-
-
-module.exports = server
+module.exports = { init, start, stop }
+if (require.main === module) {
+    if (process.argv[2] === 'start') start()
+    if (process.argv[2] === 'stop')  stop()
+}
